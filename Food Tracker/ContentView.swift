9e4ec7f0ce61a03aa8682a69634174a5720ab051
Jose <jose.salinas88@gmail.com>
2019-10-05 23:13:41 -0400

    First Commit

---
+++ Food Tracker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Food Tracker
+//
+//  Created by Jose Salinas on 10/4/19.
+//  Copyright © 2019 Jose Salinas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Eyyy YOOOOOO")
+                .font(.title)
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
